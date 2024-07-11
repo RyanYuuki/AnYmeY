6c72d1cd56b122edd3f1229aa8c8f42dc2c1e3b1
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2024-07-11 23:08:14 +0530

    fixed error boundary and recursive error

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,39 +1,34 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { Link, useRouteError } from "react-router-dom";
-import errorImage from "../assets/error.jpg";
+import errorImage from "../assets/404.png";
 export default function Error() {
   return (
     <div style={styles.container}>
       <img style={styles.error} src={errorImage} />
-      <button style={styles.button} ><Link to={'/'}>Go To HomePage</Link></button>
+      <button style={styles.button} ><a href="/home">Go To HomePage</a></button>
     </div>
   );
 }
 
 const styles = {
   container: {
-    height: "100vh",
-    width: "100vw",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
     flexDirection: "column",
-    padding: "2rem",
   },
   error : {
-    height: "100vh",
     objectFit: 'cover',
-    width : "100vw"
+    width: 'fit-content',
+    height: '600px',
   },
   button : {
-    position: 'absolute',
     padding: '10px 20px',
     backgroundColor: 'deeppink',
     color: '#fff',
     border: 'none',
     borderRadius: '10px',
-    marginTop : '25%',
-    marginLeft: '3%'
+    marginLeft: '30px',
   }
 };
