f21a62a2d490289fa9d38bd0d79e1732c3991e76
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2024-07-12 16:05:56 +0530

    - Created Manga Page

--- src/pages/Manga/Home.jsx
+++ src/pages/Manga/Home.jsx
@@ -1,13 +1,24 @@
-import Carousel from '../../components/Home/Carousel'
-import AnimeTable from '../../components/Home/AnimeTable'
-import TrendingCarousel from '../../components/Home/TrendingCarousel';
+import Carousel from "../../components/Home/Carousel";
+import AnimeTable from "../../components/Home/AnimeTable";
+import TrendingCarousel from "../../components/Home/TrendingCarousel";
+import { useEffect } from "react";
 
 export default function MangaHome() {
+  useEffect(() => {
+    document.title = "AnymeY - Manga";
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    return () => {
+      document.title = "AnymeY";
+    };
+  });
+
   return (
     <>
-    <Carousel isManga={true} />
-    <TrendingCarousel/>
-    <AnimeTable/>
+      <div className="manga-body">
+        <Carousel isManga={true} />
+        <TrendingCarousel isManga={true} />
+        <AnimeTable isManga={true} />
+      </div>
     </>
-  )
+  );
 }
