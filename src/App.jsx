e75a1bb1eb2e68d5ffdc097b5dae056995daada7
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2024-07-14 01:02:46 +0530

    - Switching back to browser router

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useThemeColors } from "./constants/Colors";
 import Header from "./components/General/Header";
 import AnimeHome from './pages/Anime/Home';
 import AnimeDetails from './pages/Anime/AnimeDetails';
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Error from "./pages/Error";
 import Streaming from "./pages/Anime/Streaming";
 import Search from './pages/Search';
@@ -13,12 +13,13 @@ import ErrorBoundary from './ErrorBoundary';
 import MangaDetails from "./pages/Manga/MangaDetails";
 import Read from "./pages/Manga/Read";
 import Home from "./pages/Home";
+
 function App() {
   const { theme, setTheme } = fetchTheme();
   const themeColors = useThemeColors();
 
   return (
-    <HashRouter>
+    <Router>
       <Header />
       <ErrorBoundary>
         <Routes>
@@ -27,14 +28,14 @@ function App() {
           <Route path="/anime/:id" element={<AnimeDetails />} />
           <Route path="/watch/:id" element={<Streaming />} />
           <Route path="/manga/home" element={<MangaHome />} />
-          <Route path="/manga/details/:id" element={<MangaDetails/>} />
+          <Route path="/manga/details/:id" element={<MangaDetails />} />
           <Route path="/manga/read/:id/:method?/:mangaId?/:mangaChapter?" element={<Read />} />
           <Route path="/search/:query?" element={<Search />} />
           <Route path="/error" element={<Error />} />
           <Route path="*" element={<Error />} />
         </Routes>
       </ErrorBoundary>
-    </HashRouter>
+    </Router>
   );
 }
 
