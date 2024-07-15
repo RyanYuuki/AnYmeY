db28dd52c3b38ce84e5d9d4440ebaf697604e9d0
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2024-07-15 23:49:08 +0530

    - Styled Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,28 @@
 import "./css/Home.css";
-import animeImage from "../assets/anime.jpg";
-import mangaImage from "../assets/manga.jpg";
+import animeImage from "../assets/homepage_cover.jpg";
+import mangaImage from "../assets/second_cover_image.webp";
 import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div className="home-body">
-      <div className="home-container">
+      <div className="left-side">
+        <img className="anime-bg" src={animeImage} alt="Anime background" />
+        <img className="manga-bg" src={mangaImage} alt="Manga background" />
+      </div>
+      <div className="right-side">
         <h1>
           Welcome to An<span>Y</span>mey
         </h1>
-        <p>Your one-stop solution for finding anime, manga, and movies</p>
+        <span>Your one-stop solution for finding anime, manga, and movies.</span>
         <div className="button-group">
-          <button className="Anime"><Link to={'/anime/home'} >Anime</Link></button>
-          <button className="Manga"><Link to={'/manga/home'} >Manga</Link></button>
+          <button className="Anime">
+            <Link to={"/anime/home"}>Anime</Link>
+          </button>
+          <button className="Manga">
+            <Link to={"/manga/home"}>Manga</Link>
+          </button>
         </div>
-        <img
-          className="bg-image anime-bg"
-          src={animeImage}
-          alt="Anime background"
-        />
-        <img
-          className="bg-image manga-bg"
-          src={mangaImage}
-          alt="Manga background"
-        />
       </div>
     </div>
   );
